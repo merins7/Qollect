5d0da542d2a5b2654f02162ddc7e00b88dfad2ba
mehga <kumarimegha1808@gmail.com>
2025-02-20 10:26:47 +0530

    Initial commit: Qollect app

--- config/firebaseConfig.jsx
+++ config/firebaseConfig.jsx
@@ -5,16 +5,15 @@ import { getFirestore } from "firebase/firestore";
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { getStorage } from 'firebase/storage';
 import { Platform } from 'react-native';
-import Constants from 'expo-constants';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "AIzaSyAeA2651ERJ8WW7Paiqju26N2k59hy7JQY",
+  authDomain: "projects-2025-bdf05.firebaseapp.com",
+  projectId: "projects-2025-bdf05",
+  storageBucket: "projects-2025-bdf05.appspot.com",
+  messagingSenderId: "400835137233",
+  appId: "1:400835137233:web:6fbc92765b129e9b557ff0"
 };
 
 // Initialize Firebase
